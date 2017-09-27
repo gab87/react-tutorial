5b6d2d0832a603b3d4f15b1b59e72fffc7d7eb60
Gabriele Marconetta <gabriele.marconetta@gmail.com>
2017-09-27 12:39:14 +0200

    avanzamento tutorial - why immutability is important

--- test/src/index.js
+++ test/src/index.js
@@ -5,16 +5,34 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-        <button className="square">
-        {/* TODO */}
+        <button className="square" onClick={() => this.props.onClick() }>
+          {this.props.value}
         </button>
-  );
+    );
   }
 }
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  handleClick(i) {
+    const _squares = this.state.squares.slice();
+    _squares[i] = 'X';
+    this.setState({squares: _squares});
   }
 
   render() {
@@ -22,24 +40,24 @@ class Board extends React.Component {
 
     return (
         <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-        {this.renderSquare(0)}
-    {this.renderSquare(1)}
-    {this.renderSquare(2)}
-  </div>
-    <div className="board-row">
-        {this.renderSquare(3)}
-    {this.renderSquare(4)}
-    {this.renderSquare(5)}
-  </div>
-    <div className="board-row">
-        {this.renderSquare(6)}
-    {this.renderSquare(7)}
-    {this.renderSquare(8)}
-  </div>
-    </div>
-  );
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+    );
   }
 }
 
@@ -47,15 +65,15 @@ class Game extends React.Component {
   render() {
     return (
         <div className="game">
-        <div className="game-board">
-        <Board />
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
         </div>
-        <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
-        </div>
-        </div>
-  );
+    );
   }
 }
 
