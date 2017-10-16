8253f0b531cb8ec846006631a22d165d7f25843b
Gabriele Marconetta <gabriele.marconetta@gmail.com>
2017-10-16 10:23:03 +0200

    Start from Keys

--- test/src/index.js
+++ test/src/index.js
@@ -51,14 +51,13 @@ class Game extends React.Component {
         squares: Array(9).fill(null)
       }],
       xIsNext: true
-    }
+    };
   }
 
   handleClick(i) {
     const history = this.state.history;
-    const current = history[history - 1];
+    const current = history[history.length - 1];
     const squares = current.squares.slice();
-
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -76,14 +75,12 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
-    //la callback di map ha 3 argomenti: (valore dell'elemento, l'indice dell'elemento, l'array originale)
     const moves = history.map((step, move) => {
-      const desc = move ? 'Move #' + move : 'Game Start';
-
+      const desc = move ? 'Go to move #' + move : 'Go to game start.';
       return (
-          <li>
-            <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
-          </li>
+        <li>
+          <button onClick={() => this.jumpTo(move)}> {desc} </button>
+        </li>
       );
     });
 
@@ -98,8 +95,8 @@ class Game extends React.Component {
         <div className="game">
           <div className="game-board">
             <Board
-              squares={current.squares}
-              onClick={(i) => this.handleClick(i)}
+                squares={current.squares}
+                onClick={(i) => this.handleClick(i)}
             />
           </div>
           <div className="game-info">
